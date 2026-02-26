03dbdf70ce84a80add524a6e4d70c7fec36e55fe
abubakarsaddiq001199@gmail.com <Abubakar>
2026-02-27 02:26:06 +0500

    first commit

---
+++ git-pract.py
@@ -0,0 +1,15 @@
+print("Hello D, I am doing a git pract just initially doing some chitchat from the terminal.")
+
+list1 = [1, 2, 23, 4, 6, 7, 7, 8, 344, 5]
+list2 = [3, 4, 7, 6]
+
+fnf_lst = []
+
+for i in list1:
+	for j in list2:
+		if j == i:
+			fnf_lst.append(i)
+
+print("Shhh!! I am doing git......")
+
+
